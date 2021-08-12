455412ab95d85d1b7f4974898d82e99ff24244b9
OahidZihad <oahidzihad1@gmail.com>
2021-08-12 20:20:11 +0600

    set navbar and header main component

---
+++ src/Components/Home/HeaderMain/HeaderMain.js
@@ -0,0 +1,26 @@
+import React from "react";
+import chair from "../../../images/chair.png";
+
+const HeaderMain = () => {
+  return (
+    <main className="row">
+      <div className="col-md-4 offset-md-1">
+        <h1>
+          Your New Smile <br /> Sarts Here
+        </h1>
+        <p className="text-secondary">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+          sunt necessitatibus, doloribus earum perferendis eligendi, commodi
+          quaerat debitis repellendus deserunt fugiat! Labore repellendus quam
+          ab autem adipisci culpa minima deserunt.
+        </p>
+        <button className="btn btn-primary">GET APPOINTMENT</button>
+      </div>
+      <div className="col-md-6">
+        <img src={chair} alt="" className="img-fluid" />
+      </div>
+    </main>
+  );
+};
+
+export default HeaderMain;
